c88fcda68e2c7405bf0d78b68e2d9d83026ec1f8
CayzerOK <cayzerok@gmail.com>
2020-03-27 12:00:42 +0300

    Fixed index page avatar image error

--- www/template/site/tmp_index.php
+++ www/template/site/tmp_index.php
@@ -1,6 +1,9 @@
 <div class="userList">
     <?php
     foreach ($content as $key => $user) {
+        if(!$user['avatar']) {
+            $user['avatar'] = '/assets/img/user.jpg';
+        }
         echo '
             <a href="user/'.$user['id'].'" class="user pageRootElem">
                 <img src="'.$user['avatar'].'" class="listAvatar">
